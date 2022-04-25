14402079b5ea17603c147e311769c2516ae39574
Evan <13736502769@163.com>
2022-04-25 22:38:58 +0800

    feat: review some

---
+++ src/components/LockedInput.tsx
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+
+type State = {}
+type Props = {}
+
+export default class ComponentName extends Component<State, Props> {
+    state = {}
+
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
